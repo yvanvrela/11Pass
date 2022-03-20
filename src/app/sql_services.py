e09024f44e7303a7dd4ef53941e207f71afe7aaf
Yvan Varela <varelaivan30@gmail.com>
2022-03-20 15:21:30 -0300

    Se corrigió las sentencias sql, y se agregó tests

--- src/app/sql_services.py
+++ src/app/sql_services.py
@@ -1,9 +1,27 @@
+from multiprocessing import reduction
 import sqlite3
 
 connection = sqlite3.connect('database.db')
 
 cursor = connection.cursor()
 
+def all_account() -> list:
+    """Trae todas la cuentas de la tabla"""
+
+    sql = "SELECT * FROM accounts"
+    list_accounts = cursor.execute(sql).fetchall()
+    connection.commit()
+
+    return list_accounts
+
+def end_element_account() -> list():
+    """Datos del ultimo elemento"""
+
+    sql = "SELECT * FROM accounts ORDER BY accounts.id_account DESC LIMIT 1"
+    end_element = cursor.execute(sql).fetchone()
+    connection.commit()
+
+    return end_element
 
 def add_account(name: str, password: str, page: str, description: str) -> None:
     """ Agrega los datos de la cuenta a la base de datos """
@@ -11,17 +29,29 @@ def add_account(name: str, password: str, page: str, description: str) -> None:
     sql = "INSERT INTO accounts \
             (name_element, password_element, page_element, description_element) \
             VALUES (?,?,?,?)"
-    values = (name, password, page, description)
-
+    values = name, password, page, description
     cursor.execute(sql, values)
     connection.commit()
 
+
 def update_account(account_id:int, name: str, password: str, page: str, description: str) -> None:
+    """Actualiza los datos de la cuenta"""
+
     sql = "UPDATE accounts  \
-        SET  name_element = %s, password_element = %s, \
-        page_element = %s, description_element = %s \
-        WHERE id_account = %d"
-    values = (name, password, page, description, account_id)
-    cursor.execute(sql,values)
+        SET  name_element = ?, password_element = ?, \
+        page_element = ?, description_element = ? \
+        WHERE id_account = ?"
+    values = name, password, page, description, account_id
+
+    cursor.execute(sql, values)
+
     connection.commit()
 
+
+def delete_account(account_id:int) -> None:
+    """Eliminar la cuenta, de la bd"""
+
+    sql = f"DELETE FROM accounts WHERE id_account= {account_id}"
+    cursor.execute(sql)
+
+    connection.commit()
--- src/tests/tests_main.py
+++ src/tests/tests_main.py
@@ -1,8 +1,12 @@
+from multiprocessing import connection
+from time import sleep
 from urllib import response
 from flask_testing import TestCase
 from flask import current_app, url_for
+import sqlite3
 
 from main import app
+from app.sql_services import all_account, end_element_account,add_account, update_account, delete_account
 
 
 class MainTest(TestCase):
@@ -20,4 +24,32 @@ class MainTest(TestCase):
 
     def test_app_in_test_mode(self):
         self.assertTrue(current_app.config['TESTING'])
+
+
+    def test_sql_add(self): 
+        add_account(name='prueba', password='123', page='page', description='description')
+        account_reference = f'Cuenta agregada: {end_element_account()}'
+        
+        return print(account_reference)
+        
+        
     
+    def test_sql_update(self):
+        account_id = end_element_account()[0]
+        update_account(account_id= account_id, name='editado', password='1235', page='web', description='test')
+
+        return print(f'Cuenta editada: {end_element_account()}')
+
+        
+    def test_sql_delete(self):
+        account_id = end_element_account()
+        account_reference = f'Cuenta eliminada: {account_id}'
+
+        delete_account(account_id= account_id[0])
+
+        return print(account_reference)
+
+        
+         
+
+        
