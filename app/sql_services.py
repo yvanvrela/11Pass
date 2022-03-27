67359984f746a0d46b172bffc0c18b0b9899d062
Yvan Varela <varelaivan30@gmail.com>
2022-03-27 10:44:03 -0400

    Get users

--- app/sql_services.py
+++ app/sql_services.py
@@ -5,7 +5,8 @@ connection = sqlite3.connect('database.db')
 
 cursor = connection.cursor()
 
-def get_user_by_id(user_id:int) -> list:
+
+def get_user_by_id(user_id: int) -> list:
     sql = f'SELECT * FROM users WHERE id_user = {user_id}'
     user = cursor.execute(sql).fetchone()
     connection.commit()
@@ -13,12 +14,14 @@ def get_user_by_id(user_id:int) -> list:
     return user
 
 
-def get_user_by_name(username:str) -> list:
-    sql = f'SELECT * FROM users WHERE user_name = {username}'
-    user = cursor.execute(sql).fetchone()
-    connection.commit()
+def get_user_by_name(username: str):
+    user_form_db = all_users()
 
-    return user
+    for user in user_form_db:
+        if username in user:
+            return user
+    
+    return False
 
 
 def all_users() -> list:
@@ -34,11 +37,12 @@ def all_users() -> list:
 def all_account() -> list:
     """Trae todas la cuentas de la tabla"""
 
-    sql = "SELECT * FROM accounts"
+    sql = 'SELECT * FROM accounts'
     list_accounts = cursor.execute(sql).fetchall()
     connection.commit()
 
     return list_accounts
+
 
 def end_element_account() -> list:
     """Datos del ultimo elemento"""
@@ -49,9 +53,10 @@ def end_element_account() -> list:
 
     return end_element
 
+
 def add_account(name: str, password: str, page: str, description: str) -> None:
     """ Agrega los datos de la cuenta a la base de datos """
-    
+
     sql = "INSERT INTO accounts \
             (name_element, password_element, page_element, description_element) \
             VALUES (?,?,?,?)"
@@ -60,7 +65,7 @@ def add_account(name: str, password: str, page: str, description: str) -> None:
     connection.commit()
 
 
-def update_account(account_id:int, name: str, password: str, page: str, description: str) -> None:
+def update_account(account_id: int, name: str, password: str, page: str, description: str) -> None:
     """Actualiza los datos de la cuenta"""
 
     sql = "UPDATE accounts  \
@@ -74,7 +79,7 @@ def update_account(account_id:int, name: str, password: str, page: str, descript
     connection.commit()
 
 
-def delete_account(account_id:int) -> None:
+def delete_account(account_id: int) -> None:
     """Eliminar la cuenta, de la bd"""
 
     sql = f"DELETE FROM accounts WHERE id_account= {account_id}"
--- tests/tests_main.py
+++ tests/tests_main.py
@@ -4,7 +4,7 @@ from flask import current_app, url_for
 import sqlite3
 
 from main import app
-from app.sql_services import all_account, end_element_account,add_account, get_user_by_name, update_account, delete_account, get_user_by_id
+from app.sql_services import all_account, all_users, end_element_account, add_account, get_user_by_name, update_account, delete_account, get_user_by_id
 
 
 class MainTest(TestCase):
@@ -15,46 +15,38 @@ class MainTest(TestCase):
 
         return app
 
-
     def test_app_exists(self):
         self.assertIsNotNone(current_app)
-
 
     def test_app_in_test_mode(self):
         self.assertTrue(current_app.config['TESTING'])
 
-
-    def test_sql_add(self): 
-        add_account(name='prueba', password='123', page='page', description='description')
+    def test_sql_add(self):
+        add_account(name='prueba', password='123',
+                    page='page', description='description')
         account_reference = f'Cuenta agregada: {end_element_account()}'
-        
+
         return print(account_reference)
-        
-        
-    
+
     def test_sql_update(self):
         account_id = end_element_account()[0]
-        update_account(account_id= account_id, name='editado', password='1235', page='web', description='test')
+        update_account(account_id=account_id, name='editado',
+                       password='1235', page='web', description='test')
 
         return print(f'Cuenta editada: {end_element_account()}')
 
-        
     def test_sql_delete(self):
         account_id = end_element_account()
         account_reference = f'Cuenta eliminada: {account_id}'
 
-        delete_account(account_id= account_id[0])
+        delete_account(account_id=account_id[0])
 
         return print(account_reference)
 
-
     def test_sql_get_user(self):
-        user_by_id = get_user_by_id(user_id=1)
-        user_by_name = get_user_by_name(username='ivan')
-        return f'Usuario by id: {user_by_id} \
-            Usuario by name: {user_by_name}'
-        
-        
-         
+        user_by_id = get_user_by_id(1)
+        user_by_name = get_user_by_name('ivan')
 
-        
+    def test_all_users(self):
+        users = all_users()
+        return print(users)
