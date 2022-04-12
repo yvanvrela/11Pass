248b14df64412dd4cefe6a21d524c87f8c214b70
Yvan Varela <varelaivan30@gmail.com>
2022-04-12 18:00:13 -0400

    create modal users

--- app/static/scripts.js
+++ app/static/scripts.js
@@ -65,7 +65,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     // Add a click event on various child elements to close the parent modal
-    (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+    (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button, .card-footer .exit') || []).forEach(($close) => {
         const $target = $close.closest('.modal');
 
         $close.addEventListener('click', () => {
